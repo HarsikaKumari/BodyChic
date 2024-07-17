5bdcb33a89b5e1c487b6ebc9b04ccdca583623cf
Harsika Kumari <kumariharsika8@gmail.com>
2024-07-17 14:27:05 +0530

    chore(users): show some  styles in the user index

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
--->
 <!doctype html>
 <html lang="zxx">
 
@@ -9,7 +5,7 @@ Author URL: http://w3layouts.com
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>SpryStore E-commerce Category Bootstrap Responsive Website Template | Home :: W3layouts</title>
+	<title>BodyChic Your Own Designer</title>
 	<!-- Template CSS -->
 	<link rel="stylesheet" href="assets/css/style-starter.css">
 	<!-- Template CSS -->
@@ -20,26 +16,23 @@ Author URL: http://w3layouts.com
 </head>
 
 <body>
+
 	<!--w3l-banner-slider-main-->
 	<section class="w3l-banner-slider-main">
 		<div class="top-header-content">
 			<header class="tophny-header">
 				<div class="container-fluid">
 					<div class="top-right-strip row">
-						<!--/left-->
-						<div class="top-hny-left-content col-lg-6 pl-lg-0">
-							<h6>Upto 30% off on All styles , <a href="#" target="_blank"> Click here for <span class="fa fa-hand-o-right hand-icon" aria-hidden="true"></span> <span class="hignlaite">More details</span></a></h6>
-						</div>
-						<!--//left-->
+
 						<!--/right-->
-						<ul class="top-hnt-right-content col-lg-6">
+						<ul class="top-hnt-right-content col-lg-12">
 
 							<li class="button-log usernhy">
 								<a class="btn-open" href="#">
-									<span class="fa fa-user" aria-hidden="true"></span>
+									<span class="fa fa-user" aria-hidden="true"></span> Login
 								</a>
 							</li>
-							<li class="transmitvcart galssescart2 cart cart box_1">
+							<!-- <li class="transmitvcart galssescart2 cart cart box_1">
 								<form action="#" method="post" class="last">
 									<input type="hidden" name="cmd" value="_cart">
 									<input type="hidden" name="display" value="1">
@@ -48,7 +41,7 @@ Author URL: http://w3layouts.com
 										<span class="fa fa-shopping-cart"></span>
 									</button>
 								</form>
-							</li>
+							</li> -->
 						</ul>
 						<!--//right-->
 						<div class="overlay-login text-left">
@@ -65,7 +58,7 @@ Author URL: http://w3layouts.com
 				<nav class="navbar navbar-expand-lg navbar-light">
 					<div class="container-fluid serarc-fluid">
 						<a class="navbar-brand" href="index.php">
-							Spry<span class="lohny">S</span>tore</a>
+							Body<span class="lohny">C</span>hic</a>
 						<!-- if logo is image enable this   
 							<a class="navbar-brand" href="#index.php">
 								<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
@@ -87,7 +80,7 @@ Author URL: http://w3layouts.com
 								</div>
 								<a class="close" href="#">×</a>
 							</div>
-							<!-- /search popup -->
+
 						</div>
 						<!--//search-right-->
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -116,99 +109,85 @@ Author URL: http://w3layouts.com
 				</nav>
 				<!--//nav-->
 			</header>
-			<div class="bannerhny-content">
+			<!-- <div class="bannerhny-content"> -->
 
-				<!--/banner-slider-->
-				<div class="content-baner-inf">
-					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-							<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-						</ol>
-						<div class="carousel-inner">
-							<div class="carousel-item active">
-								<div class="container">
-									<div class="carousel-caption">
-										<h3>Women's
-											Fashion
-											<br>50% Off
-										</h3>
-										<a href="#" class="shop-button btn">
-											Shop Now
-										</a>
+			<!--/banner-slider-->
+			<div class="content-baner-inf">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<div class="container">
+								<div class="carousel-caption">
+									<h3>Women's
+										Fashion
+										<br>
+									</h3>
+									<a href="#" class="shop-button btn">
+										explore More
+									</a>
 
-									</div>
-								</div>
-							</div>
-							<div class="carousel-item item2">
-								<div class="container">
-									<div class="carousel-caption">
-										<h3>Men's
-											Fashion
-											<br>60% Off
-										</h3>
-										<a href="#" class="shop-button btn">
-											Shop Now
-										</a>
-
-									</div>
-								</div>
-							</div>
-							<div class="carousel-item item3">
-								<div class="container">
-									<div class="carousel-caption">
-										<h3>Women's
-											Fashion
-											<br>50% Off
-										</h3>
-										<a href="#" class="shop-button btn">
-											Shop Now
-										</a>
-
-									</div>
-								</div>
-							</div>
-							<div class="carousel-item item4">
-								<div class="container">
-									<div class="carousel-caption">
-										<h3>Men's
-											Fashion
-											<br>60% Off
-										</h3>
-										<a href="#" class="shop-button btn">
-											Shop Now
-										</a>
-									</div>
 								</div>
 							</div>
 						</div>
-						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-					</div>
-				</div>
-				<!--//banner-slider-->
-				<!--//banner-slider-->
-				<div class="right-banner">
-					<div class="right-1">
-						<h4>
-							Men's
-							Fashion
-							<br>50% Off
-						</h4>
-					</div>
-				</div>
+						<div class="carousel-item item2">
+							<div class="container">
+								<div class="carousel-caption">
+									<h3>Men's
+										Fashion
+									</h3>
+									<a href="#" class="shop-button btn">
+										Explore More
+									</a>
 
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item item3">
+							<div class="container">
+								<div class="carousel-caption">
+									<h3>Women's
+										Fashion
+									</h3>
+									<a href="#" class="shop-button btn">
+										Explore More
+									</a>
+
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item item4">
+							<div class="container">
+								<div class="carousel-caption">
+									<h3>Men's
+										Fashion
+									</h3>
+									<a href="#" class="shop-button btn">
+										Explore More
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
 			</div>
+		</div>
 
 	</section>
+
 	<!-- //w3l-banner-slider-main -->
 	<section class="w3l-grids-hny-2">
 		<!-- /content-6-section -->
@@ -217,92 +196,59 @@ Author URL: http://w3layouts.com
 
 				<h3 class="hny-title mb-0 text-center">Shop With <span>Us</span></h3>
 				<p class="mb-4 text-center">Handpicked Favourites just for you</p>
-				<div class="welcome-grids row mt-5">
-					<div class="col-lg-2 col-md-4 col-6 welcome-image">
-						<div class="boxhny13">
-							<a href="#URL">
-								<img src="assets/images/grid1.jpg" class="img-fluid" alt="" />
-								<div class="boxhny-content">
-									<h3 class="title">Product
-								</div>
-							</a>
-						</div>
-						<h4><a href="#URL">Hand Bags</a></h4>
 
-					</div>
+				<div class="welcome-grids row mt-5">
+
+					<?php
+					//1. database connect
+					include("config.php");
+					//2. query
+					//SELECT * from `table`
+					$query = "SELECT * from `styles` limit 5";
+					//3. query run with database
+					$result = mysqli_query($connect, $query);
+					//4. result use
+					// print_r($result);
+					$sno = 1;
+					while ($data = mysqli_fetch_assoc($result)) {
+					?>
+
+						<div class="col-lg-2 col-md-4 col-6 welcome-image">
+							<div class="boxhny13">
+								<a href="#URL">
+									<img src="style_images/<?php echo $data['image'] ?>" class="img-fluid" style="height:160px;width:160px;" />
+									<div class="boxhny-content">
+										<h3 class="title"><?php echo $data['name'] ?>
+									</div>
+								</a>
+							</div>
+							<h4><a><?php echo $data['name'] ?></a></h4>
+
+						</div>
+
+					<?php
+						// $sno=$sno+1;
+						$sno++;
+					}
+					?>
 
 					<div class="col-lg-2 col-md-4 col-6 welcome-image">
 						<div class="boxhny13">
 							<a href="#URL">
 								<img src="assets/images/grid3.jpg" class="img-fluid" alt="" />
 								<div class="boxhny-content">
-									<h3 class="title">Product</h3>
+									<h3 class="title">More</h3>
 								</div>
 							</a>
 						</div>
-						<h4><a href="#URL">
-								Watches</a></h4>
-
-
-					</div>
-					<div class="col-lg-2 col-md-4 col-6 welcome-image">
-						<div class="boxhny13">
-							<a href="#URL">
-								<img src="assets/images/grid2.jpg" class="img-fluid" alt="" />
-								<div class="boxhny-content">
-									<h3 class="title">Product</h3>
-								</div>
-							</a>
-						</div>
-						<h4><a href="#URL">Shoes</a></h4>
-
-
-					</div>
-					<div class="col-lg-2 col-md-4 col-6 welcome-image">
-						<div class="boxhny13">
-							<a href="#URL">
-								<img src="assets/images/grid4.jpg" class="img-fluid" alt="" />
-								<div class="boxhny-content">
-									<h3 class="title">Product</h3>
-								</div>
-							</a>
-						</div>
-						<h4><a href="#URL">Suits</a></h4>
-
-					</div>
-					<div class="col-lg-2 col-md-4 col-6 welcome-image">
-						<div class="boxhny13">
-							<a href="#URL">
-								<img src="assets/images/grid5.jpg" class="img-fluid" alt="" />
-								<div class="boxhny-content">
-									<h3 class="title">Product</h3>
-								</div>
-							</a>
-						</div>
-						<h4><a href="#URL">
-								Accessories</a></h4>
-
-
-					</div>
-					<div class="col-lg-2 col-md-4 col-6 welcome-image">
-						<div class="boxhny13">
-							<a href="#URL">
-								<img src="assets/images/grid6.jpg" class="img-fluid" alt="" />
-								<div class="boxhny-content">
-									<h3 class="title">Product</h3>
-								</div>
-							</a>
-						</div>
-						<h4><a href="#URL">
-								Fragrances</a></h4>
-
-
+						<h4><a>Explore More &rarr;</a></h4>
 					</div>
 				</div>
 
 			</div>
 		</div>
 	</section>
+
 	<!-- //content-6-section -->
 
 	<section class="w3l-content-w-photo-6">
@@ -705,7 +651,7 @@ Author URL: http://w3layouts.com
 							<ul class="blog-info-list">
 								<li><a href="#admin">By admin</a></li>
 								<li class="date-post">
-									Aug 10, 2020</li>
+									Aug 10, 2024</li>
 							</ul>
 							<h4><a href="#">Here to bring your life style to the next level.</a></h4>
 						</div>
