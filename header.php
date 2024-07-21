fbf74c32ee64cce4e8d572a65fdbd450c707b4b7
Harsika Kumari <kumariharsika8@gmail.com>
2024-07-21 22:19:32 +0530

    feat(admin): remove and rename unneccesary search bar from admin panel's header

--- header.php
+++ header.php
@@ -38,57 +38,39 @@
                     <!--/nav-->
                     <nav class="navbar navbar-expand-lg navbar-light">
                         <div class="container-fluid serarc-fluid">
-                            <a class="navbar-brand" href="index.php">
-                                <div class="search-right">
 
-                                    <div id="search" class="pop-overlay">
-                                        <div class="popup">
+                            <!--//search-right-->
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon fa fa-bars"> </span>
+                            </button>
 
-                                            <form action="#" method="post" class="search-box">
-                                                <input type="search" placeholder="Keyword" name="search" required="required" autofocus="">
-                                                <button type="submit" class="btn">Search</button>
-                                            </form>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="category.php">Category</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="styles.php">Styles</a>
+                                    </li>
 
-                                        </div>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="color.php">Colors</a>
+                                    </li>
 
-                                        <a class="close" href="#">×</a>
-                                    </div>
-                                    <a href="#search" title="search"><span class="fa fa-search mr-2" aria-hidden="true"></span>
-                                        <span class="search-text">Search here</span></a>
-                                    <!-- /search popup -->
-                                </div>
-                                <!--//search-right-->
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon fa fa-bars"> </span>
-                                </button>
+                                    <!-- <ul class="top-hnt-right-content col-lg-6"> -->
 
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul class="navbar-nav ml-auto">
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="index.php">Home</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="about.php">About</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="about.php">What We Offer</a>
-                                        </li>
+                                    <li class="nav-item button-log ">
+                                        <a class="nav-link " href="#">
+                                            <span class="fa fa-user" aria-hidden="true"></span> Login
+                                        </a>
+                                    </li>
+                                    <!-- </ul> -->
+                                </ul>
 
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="contact.php">Contact</a>
-                                        </li>
-
-                                        <!-- <ul class="top-hnt-right-content col-lg-6"> -->
-
-                                        <li class="nav-item button-log usernhy">
-                                            <a class="nav-link btn-open" href="#">
-                                                <span class="fa fa-user" aria-hidden="true"></span>
-                                            </a>
-                                        </li>
-                                        <!-- </ul> -->
-                                    </ul>
-
-                                </div>
+                            </div>
                         </div>
                     </nav>
                     <!--//nav-->
