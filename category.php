2c3c5acb83504e5babff22e034127917de90fa90
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-09 23:23:17 +0530

    feat(category): add a feature of dropdown for better ux

--- category.php
+++ category.php
@@ -1,17 +1,22 @@
 <?php
 include("header.php");
-include("session_check.php")
+if (isset($_SESSION["gender"])) {
+    $gender = $_SESSION["gender"];
+}
+if (isset($_REQUEST["search"])) {
+    $gender = $_REQUEST["gender"];
+}
 ?>
 
 <div class="breadcrumb-contentnhy">
     <div class="container">
         <nav aria-label="breadcrumb">
-            <h2 class="hny-title text-center">color Us</h2>
+            <h2 class="hny-title text-center">Types</h2>
             <ol class="breadcrumb mb-0">
                 <li><a href="index.php">Home</a>
                     <span class="fa fa-angle-double-right"></span>
                 </li>
-                <li class="active">color</li>
+                <li class="active">Types</li>
             </ol>
         </nav>
     </div>
@@ -27,38 +32,77 @@ include("session_check.php")
             <h3 class="hny-title mb-0 text-center">Explore more <span>Category</span></h3>
             <!-- <p class="mb-4 text-center"></p> -->
 
-            <div class="welcome-grids row mt-5">
 
-                <?php
-                //1. database connect
-                include("config.php");
-                //2. query
-                //SELECT * from `table`
-                $query = "SELECT * from `category`";
-                //3. query run with database
-                $result = mysqli_query($connect, $query);
-                //4. result use
-                // print_r($result);
-                $sno = 1;
-                while ($data = mysqli_fetch_assoc($result)) {
-                ?>
+            <form method="post">
+                <div class="row my-2">
+                    <div class="col-md-2 offset-md-3">
+                        <label>Change Gender</label>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group">
+                            <select class="form-control" name="gender">
+                                <?php
+                                $gender_arr = ["All", "Both", "Male", "Female"];
+                                foreach ($gender_arr as $gen) {
+                                ?>
+                                    <option
+                                        <?php
+                                        if (isset($gender)) {
+                                            if ($gender == $gen) {
+                                                echo "Selected";
+                                            }
+                                        }
+                                        ?>><?php echo $gen ?></option>
+                                <?php
+                                }
+                                ?>
 
-                    <div class="col-lg-2 col-md-4 col-6 welcome-image">
-                        <div class="boxhny13">
-                            <a href="#URL">
-                                <img src="category_images/<?php echo $data['thumbnail'] ?>" class="img-fluid" style="height:160px;width:160px;" />
-                                <div class="boxhny-content">
-                                    <h3 class="title"><?php echo $data['category_name'] ?>
-                                </div>
-                            </a>
+                            </select>
+                            <button class="btn btn-primary input-group-text" name="search">Change</button>
                         </div>
-                        <h4><a><?php echo $data['category_name'] ?></a></h4>
 
                     </div>
+                </div>
+            </form>
 
+            <div class="welcome-grids row mt-5">
                 <?php
-                    // $sno=$sno+1;
-                    $sno++;
+                include("config.php");
+
+                if (isset($gender)) {
+                    if ($gender == "All") {
+                        $query = "SELECT * from `category`";
+                    } else {
+                        $query = "SELECT * from `category` where `gender`='$gender'";
+                    }
+                } else {
+                    $query = "SELECT * from `category`";
+                }
+                $result = mysqli_query($connect, $query);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($data = mysqli_fetch_assoc($result)) {
+                ?>
+
+                        <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                            <div class="boxhny13">
+                                <a href="styles.php?category=<?php echo $data['id'] ?>">
+                                    <img src="category_images/<?php echo $data['thumbnail'] ?>" class="img-fluid" style="height:160px;width:160px;" />
+                                    <div class="boxhny-content">
+                                        <h3 class="title"><?php echo $data['category_name'] ?>
+                                    </div>
+                                </a>
+                            </div>
+                            <h4><a><?php echo $data['category_name'] ?></a></h4>
+
+                        </div>
+
+                    <?php
+
+                    }
+                } else {
+                    ?>
+                    <h1>No category found!!</h1>
+                <?php
                 }
                 ?>
 
