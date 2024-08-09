c689b1971d13517eb94a920ae6940bbc8409e986
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-09 23:33:30 +0530

    refactor(images): change images

--- Db/bodychic.sql
+++ Db/bodychic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2024 at 06:10 AM
+-- Generation Time: Aug 03, 2024 at 05:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,8 +32,15 @@ CREATE TABLE `admin` (
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Active',
-  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `status`, `created_at`) VALUES
+(2, 'Kumariharsika8@gmail.com', '8ae61814b4db2085b569db508393420d', 'Active', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -46,7 +53,7 @@ CREATE TABLE `category` (
   `category_name` varchar(50) NOT NULL,
   `thumbnail` varchar(50) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Active',
-  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -54,8 +61,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `category_name`, `thumbnail`, `status`, `created_at`) VALUES
-(19, 'Dresses', '1575863561banner1.jpg', 'Active', '2024-07-09 07:46:00.772728'),
-(20, 'Hugging Dresses', '1640776434banner1.jpg', 'Active', '2024-07-09 07:47:08.112206');
+(19, 'Dresses', '1575863561banner1.jpg', 'Active', '2024-07-09 07:46:00'),
+(20, 'Hugging Dresses', '1640776434banner1.jpg', 'Active', '2024-07-09 07:47:08');
 
 -- --------------------------------------------------------
 
@@ -70,7 +77,7 @@ CREATE TABLE `colors` (
   `details` varchar(50) NOT NULL,
   `tone` varchar(20) NOT NULL,
   `status` varchar(12) NOT NULL DEFAULT 'Active',
-  `created_at` timestamp(6) NULL DEFAULT NULL
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -95,7 +102,7 @@ CREATE TABLE `styles` (
   `category` varchar(20) NOT NULL,
   `details` varchar(50) NOT NULL,
   `status` varchar(12) NOT NULL DEFAULT 'Active',
-  `created_at` varchar(6) NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -103,11 +110,11 @@ CREATE TABLE `styles` (
 --
 
 INSERT INTO `styles` (`id`, `name`, `image`, `bodytype`, `gender`, `category`, `details`, `status`, `created_at`) VALUES
-(4, 'westerns', '714907084grid1.jpg', 'All', 'Female', 'Hand bags', 'Indain wear for collage and schools', 'Active', '2024-0'),
-(5, 'westerns', '604404310left1.jpg', 'Hourglass', 'Female', 'Dresses', 'Western wear for collage and schools', 'Active', '2024-0'),
-(6, 'Casual', '417582758styleCasula.jpg', 'Hourglass', 'Female', 'Dress', 'Casual outfits for office and schools.', 'Active', '2024-0'),
-(7, 'Sporty', '1179198209indexSporty.jpeg', 'All', 'Female', 'Dresses', 'Sporty dresses for gym and yoga.', 'Active', '2024-0'),
-(8, 'Indian', '105639344shop-4.jpg', 'All', 'Female', 'Dress', 'Indain wear for collage and schools', 'Active', '2024-0');
+(4, 'westerns', '714907084grid1.jpg', 'All', 'Female', 'Hand bags', 'Indain wear for collage and schools', 'Active', '0000-00-00 00:00:00'),
+(5, 'westerns', '604404310left1.jpg', 'Hourglass', 'Female', 'Dresses', 'Western wear for collage and schools', 'Active', '0000-00-00 00:00:00'),
+(6, 'Casual', '417582758styleCasula.jpg', 'Hourglass', 'Female', 'Dress', 'Casual outfits for office and schools.', 'Active', '0000-00-00 00:00:00'),
+(7, 'Sporty', '1179198209indexSporty.jpeg', 'All', 'Female', 'Dresses', 'Sporty dresses for gym and yoga.', 'Active', '0000-00-00 00:00:00'),
+(8, 'Indian', '105639344shop-4.jpg', 'All', 'Female', 'Dress', 'Indain wear for collage and schools', 'Active', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -128,7 +135,7 @@ CREATE TABLE `users` (
   `Body_shape` varchar(12) NOT NULL,
   `colorTone` varchar(12) NOT NULL,
   `status` varchar(12) NOT NULL DEFAULT 'Active',
-  `crated_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `crated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -136,10 +143,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `bust_size`, `waist_size`, `high_hip_size`, `hip_size`, `Body_shape`, `colorTone`, `status`, `crated_at`) VALUES
-(2, 'Harsika', 'kumariharsika8@gmail.com', '202cb962ac59075b964b', 7883447773, 0, 0, 0, 0, '0', '', 'Active', '2024-07-19 10:16:22.035449'),
-(4, 'sheetal', 'sheetal02@gmail.com', '81dc9bdb52d04dc20036', 7883448973, 0, 0, 0, 0, '0', '', 'Active', '2024-07-19 19:02:21.228826'),
-(5, 'sheetal kri', 'kumarishetal@gmail.com', '81dc9bdb52d04dc20036', 782467554, 0, 0, 0, 0, '0', '', 'Active', '2024-07-19 19:17:49.224204'),
-(9, 'Harsika Kumari', 'harsika@lpu.in', '81dc9bdb52d04dc20036', 7883447773, 0, 0, 0, 0, '', '', 'Active', '2024-07-20 04:04:49.493047');
+(2, 'Harsika', 'kumariharsika8@gmail.com', '202cb962ac59075b964b', 7883447773, 34, 34, 33, 43, 'Rectangle', '', 'Active', '2024-07-19 10:16:22'),
+(4, 'sheetal', 'sheetal02@gmail.com', '81dc9bdb52d04dc20036', 7883448973, 0, 0, 0, 0, '0', '', 'Active', '2024-07-19 19:02:21'),
+(5, 'sheetal kri', 'kumarishetal@gmail.com', '81dc9bdb52d04dc20036', 782467554, 0, 0, 0, 0, '0', '', 'Active', '2024-07-19 19:17:49'),
+(12, 'Harsika Kumari', 'harsika@lpu.in', '81dc9bdb52d04dc20036', 7883447773, 0, 0, 0, 0, '', '', 'Active', '2024-07-20 06:42:56'),
+(13, 'prithvi', 'helo02@gmail.com', 'e10adc3949ba59abbe56', 78877433, 0, 0, 0, 0, '', '', 'Active', '2024-07-20 09:39:48'),
+(14, 'hello', 'Hello2@gmail.com', '81b073de9370ea873f54', 765788332, 0, 0, 0, 0, '', '', 'Active', '2024-07-20 09:46:29'),
+(15, 'harsikas', 'kumari2@gmail.com', 'e10adc3949ba59abbe56', 483647687, 0, 0, 0, 0, '', '', 'Active', '2024-07-20 09:49:01');
 
 --
 -- Indexes for dumped tables
@@ -183,7 +193,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -198,16 +208,10 @@ ALTER TABLE `colors`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `styles`
---
-ALTER TABLE `styles`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
